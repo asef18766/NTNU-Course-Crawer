b5d0e6259b944e451d2add6864c07780697c33c1
bogay <pojay11523@gmail.com>
2020-06-26 21:17:21 +0800

    Ignore .DS_Store

---
+++ NTNU Course Crawer/Components/NButton.swift
@@ -0,0 +1,21 @@
+//
+//  NButton.swift
+//  NTNU Course Crawer
+//
+//  Created by Bogay Chuang on 2020/6/26.
+//  Copyright © 2020 Normal OJ. All rights reserved.
+//
+
+import SwiftUI
+
+struct NButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NButton_Previews: PreviewProvider {
+    static var previews: some View {
+        NButton()
+    }
+}
---
+++ NTNU Course Crawer/Components/NLabelTextField.swift
@@ -0,0 +1,21 @@
+//
+//  NLabelTextField.swift
+//  NTNU Course Crawer
+//
+//  Created by Bogay Chuang on 2020/6/26.
+//  Copyright © 2020 Normal OJ. All rights reserved.
+//
+
+import SwiftUI
+
+struct NLabelTextField: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NLabelTextField_Previews: PreviewProvider {
+    static var previews: some View {
+        NLabelTextField()
+    }
+}
